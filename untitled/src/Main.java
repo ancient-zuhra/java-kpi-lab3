5635b0f052197cdd9c40fd1e38aa36ef03585812
nikitadmytriienko <profinisher132@gmail.com>
2023-10-25 21:48:59 +0300

    cleanup

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -6,34 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         double[][] points = {{1,0,6}, {0,1,0}, {0,0,1}};
-//        BlackTriangle blackTriangle = new BlackTriangle(points);
-//        WhiteTriangle whiteTriangle = new WhiteTriangle(points);
-//
-//
-//        double[] point = {1,0,0};
-//        blackTriangle.SetPoint(point, 0);
-//
-//        WhiteCircle whiteCircle = new WhiteCircle(1.0, 0.0, 4.0);
-//
-//        System.out.println(blackTriangle);
-//        System.out.println(whiteTriangle);
-//
-//        System.out.println(blackTriangle.GetArea());
-//        System.out.println(whiteTriangle.GetArea());
-//
-//        System.out.println(blackTriangle.GetMedianPoint(0)[2]);
-//        System.out.println(whiteTriangle.GetMedianPoint(0)[1]);
-//
-//        System.out.println(whiteCircle);
         Client client = new Client(new WhiteFactory());
-//        client.print();
 
         client.setCircle(1.0, 0.0, 4.0);
         client.setTriangle(points);
         client.print();
 
         Client client2 = new Client(new BlackFactory());
-//        client.print();
 
         client2.setCircle(1.0, 0.0, 4.0);
         client2.setTriangle(points);
